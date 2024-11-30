c01dd1345543351eb42e148e5f7cded0304a19dc
WSommerfeld <marcinlasak5@gmail.com>
2024-11-30 17:38:58 +0100

    up

--- .venv/dbbasic.py
+++ .venv/dbbasic.py
@@ -9,7 +9,7 @@ def connect(name):
 def execute(conn, query):
     try:
         cur = conn.cursor()
-        cur.execute(query)
+        result = cur.execute(query)
         conn.commit()
     except sqlite3.OperationalError as e1:
         if "syntax error" in e1.args[0]:
@@ -17,7 +17,7 @@ def execute(conn, query):
         if "already exists" in e1.args[0]:
             print("Tabela już istnieje!")
 
-
+    return result
 
 
 #robots create
@@ -73,20 +73,66 @@ def select(conn, col, table):
 def insertRobots(conn, model, type, warranty_number):
     execute(conn,"insert into robots values("+str(nextid(conn,"Robots")) +", '" +model + "', '" +type+ "', '"+ str(serialnumber(conn))+"', '"+warranty_number+"')")
 
+#oblicznie id
 def nextid(conn,table):
-    id = select(conn, "count(robot_id)", table)[0][0]+1
+    if(table == "Robots"):
+        if(select(conn,"count(robot_id)",table)[0][0]>0):
+            id = select(conn, "max(robot_id)", table)[0][0]+1
+        else:
+            id=0
+    if(table == "Availability"):
+        if (select(conn, "count(robot_id)",table)[0][0] != None):
+            id = select(conn, "max(availability_id)", table)[0][0]+1
+        else:
+            id = 0
     return id
 
+#symulacja nr seryjnego
 def serialnumber(conn):
     #tymczasowo
     #wsm to można by to ręcznie wprowadzać ale do testowania aktualnie to
     #zbyt duży ból w dupie
-    if select(conn, "count(serial_number)", "Robots")==None:
-        num=2137420
+    if select(conn, "count(robot_id)", "Robots")[0][0]>0:
+        num = select(conn, "max(robot_id)", "Robots")[0][0] + 2137420+1
     else:
-        num = select(conn, "count(serial_number)", "Robots")[0][0] + 2137420
+        num=2137420
     return num
 
+#model
+def RgetModel(conn, id):
+    model = execute(conn,"select model from Robots where robot_id="+ str(id)).fetchone()[0]
+    return model
+
+# typ
+def RgetType(conn, id):
+    type = execute(conn,"select type from Robots where robot_id=" + str(id)).fetchone()[0]
+    return type
+
+# numer seryjny
+def RgetSerial(conn, id):
+    serial = execute(conn,"select serial_number from Robots where robot_id=" + str(id)).fetchone()[0]
+    return serial
+
+#numer gwarancji
+def RgetWarranty(conn, id):
+    warranty=execute(conn,"select warranty_number from Robots where robot_id=" + str(id)).fetchone()[0]
+    return warranty
+
+#ilosc robotów
+def RgetQuantity(conn):
+    q=select(conn, "count(robot_id)","Robots")
+    return q[0][0]
+
+#usun robota
+def DeleteRobot(conn, id):
+    execute(conn,"delete from Robots where robot_id=" + str(id))
+
+def RgetWhere(conn, col,value,):
+    if type(value) == str:
+        result = execute(conn,"select robot_id from Robots where "+col+"="+"'"+value+"'")
+    else:
+        result = execute(conn,"select robot_id from Robots where "+col+"="+str(value))
+    return result
 
 def insertAvailability(conn, robot_id, status, end_date, price):
     execute(conn, "insert into Availability values("+str(nextid(conn, "Availability"))+", "+str(robot_id)+", '"+status+"', "+end_date+ ", "+str(price)+") ")
--- .venv/main.py
+++ .venv/main.py
@@ -1,6 +1,6 @@
 
 import dbbasic as db
-from dbbasic import insertRobots
+from dbbasic import insertRobots, execute, select
 
 con=db.connect("test1.db")
 #db.create_table_Robots(con)
@@ -9,8 +9,9 @@ con=db.connect("test1.db")
 #db.execute(con, "insert into robots values(12345, 'model1', 'typ1', '54321', '12543')")
 
 
-db.insertRobots(con, "model1","sprzatanie","32819")
+db.insertRobots(con, "model2","czyszczenie","32819")
 db.insertAvailability(con,  2, "git", "2024-06-06", 231)
 
 
+rows = db.RgetWhere(con,"model","model2")
 
