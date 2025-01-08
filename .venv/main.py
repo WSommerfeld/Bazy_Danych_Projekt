d5c50fb6ea9eb22da997c379b302bec8d9f68aab
WSommerfeld <marcinlasak5@gmail.com>
2025-01-08 22:13:53 +0100

    x

--- .venv/main.py
+++ .venv/main.py
@@ -8,15 +8,5 @@ con=db.connect("DATA.db")
 
 GUI.start_gui()
 
-row = db.select(con,"*", "robots")
 
-for i in range(69):
 
-    typ= ["sprzątanie","malowanie","rozrywka","muzyka","ogrodnictwo"]
-
-    x=random.randint(0,4)
-    y=random.randint(0,420)
-
-    db.insertRobots(con,"model"+str(i),typ[x],"S-"+str(i)+"-R-"+str(y)+"-T")
-
-db.printresult(select(con,"*","robots"))
